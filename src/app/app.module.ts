ee1056dbe24cdd44ca9d3ebda98c4c624bbdacbb
Dhanvant_Marathe <marathe8459@gmail.com>
2024-09-05 09:13:09 +0530

    Initially committed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MenuTreeComponent } from './menu-tree/menu-tree.component';
+
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MenuTreeComponent,  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatDialogModule,
+    MatTreeModule, MatButtonModule, MatIconModule
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/menu-tree/menu-tree.component.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-tree',
+  templateUrl: './menu-tree.component.html',
+  styleUrls: ['./menu-tree.component.css']
+})
+export class MenuTreeComponent implements OnInit {
+  menuData: any[] = [];
+  rootElements: any[] = [];
+  selectedNode: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  // ngOnInit(): void {
+  //   this.http.get('http://49.249.110.2:8050/api/MenuMasters/GetMenuMasterList/173')
+  //     .subscribe((data: any) => {
+  //       this.menuData = data;
+  //       console.log(this.menuData);
+  //       if (Array.isArray(this.menuData)) {
+  //         this.rootElements = this.menuData.filter((item: any) => !item.refMenuId);
+  //       } else {
+  //         console.error('Unexpected response format:', this.menuData);
+  //       }
+        
+  //       this.rootElements = this.menuData.filter((item: any) => !item.refMenuId); // Root elements have no parent
+  //     });
+  // }
+
+  ngOnInit(): void {
+    this.http.get('http://49.249.110.2:8050/api/MenuMasters/GetMenuMasterList/173')
+    .subscribe((response: any) => {
+      console.log(response); // Inspect the structure of the response
+      this.menuData = response.data; // Adjust according to actual response structure
+      
+      if (Array.isArray(this.menuData)) {
+        this.rootElements = this.menuData.filter((item: any) => !item.refMenuId);
+      } else {
+        console.error('Unexpected response format:', this.menuData);
+      }
+    });
+  
+  }
+ 
+
+
+  onRowClick(node: any): void {
+    this.selectedNode = node;
+    // Find all child elements
+    this.selectedNode.children = this.menuData.filter(item => item.refMenuId === node.id);
+  }
+
+  closePopup(): void {
+    this.selectedNode = null;
+  }
+}
